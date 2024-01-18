55d68412447375e31f2565e3afcb8933aaf64803
woo3145 <lcwoo3145@gmail.com>
2024-01-19 02:46:11 +0900

    ✨ Monta - 메인페이지 작업

--- src/app/(main)/timer/page.tsx
+++ src/app/(main)/timer/page.tsx
@@ -1,10 +1,47 @@
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
 import Image from 'next/image';
 
-export default function Home() {
+export default function Timer() {
   return (
-    <div className="relative w-full h-full flex flex-col items-center justify-center">
-      <div className="px-12">
-        <Image src="/monta.png" width={400} height={400} alt="monta-main" />
+    <div className="w-full h-full py-4 overflow-y-scroll scrollbar-hide">
+      <p className="text-sm text-center text-muted-foreground">
+        tip. 공부를 하면 보유중인 알들을 부화시킬 수 있어요
+      </p>
+      <div className="px-10">
+        <Image width={500} height={500} src="/orange.png" alt="orange" />
+      </div>
+      <p className="text-center text-sm font-bold pb-4">
+        보유중인 알 (최대 4개)
+      </p>
+      <div className="grid grid-cols-4 px-4">
+        <Button
+          variant={'ghost'}
+          className="h-auto p-1 flex flex-col items-center justify-center text-xs font-semibold"
+        >
+          <Image width={200} height={200} src="/egg_1.png" alt="orange" />
+          <p>00:00:00</p>
+        </Button>
+        <Button
+          variant={'ghost'}
+          className="h-auto p-1 flex flex-col items-center justify-center text-xs font-semibold"
+        >
+          <Image width={200} height={200} src="/egg_1.png" alt="orange" />
+          <p>04:43:22</p>
+        </Button>
+        <Button
+          variant={'ghost'}
+          className="h-auto p-1 flex flex-col items-center justify-center text-xs font-semibold"
+        >
+          <Image width={200} height={200} src="/egg_1.png" alt="orange" />
+          <p>02:41:08</p>
+        </Button>
+        <Button
+          variant={'ghost'}
+          className="h-auto p-1 flex flex-col items-center justify-center text-xs font-semibold"
+        >
+          <Plus />
+        </Button>
       </div>
     </div>
   );
