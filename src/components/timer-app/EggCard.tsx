dd4c21ae0481e6382bede314447e7a1e6ae07c22
woo3145 <lcwoo3145@gmail.com>
2024-01-19 17:33:18 +0900

    ✨ Monta - EggCard 오픈 가능여부 표시

--- src/components/timer-app/EggCard.tsx
+++ src/components/timer-app/EggCard.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Button } from '../ui/button';
 import { formatTime } from '@/lib/date-format';
+import { cn } from '@/lib/utils';
 
 interface Props {
   imgSrc: string;
@@ -9,11 +10,16 @@ interface Props {
 }
 
 export const EggCard = ({ imgSrc, alt = 'egg_card', restSeconds }: Props) => {
+  const isActive = restSeconds === 0;
   return (
     <Button
-      variant={'ghost'}
-      className="h-auto p-1 flex flex-col items-center justify-center text-xs font-semibold"
+      variant={isActive ? 'default' : 'ghost'}
+      className={cn(
+        'h-auto p-1 flex flex-col items-center justify-center text-xs font-semibold',
+        isActive && 'hover:-translate-y-1 duration-200 transition-transform'
+      )}
     >
+      {isActive ? <p>Open</p> : null}
       <Image width={200} height={200} src={imgSrc} alt={alt} />
       <p>{formatTime(restSeconds)}</p>
     </Button>
