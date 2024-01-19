c915595ddbcc3af562a4cbed0c49ea59e1ae0738
woo3145 <lcwoo3145@gmail.com>
2024-01-19 17:44:42 +0900

    ✨ Monta - 스트릭 Section 작업(임시)

--- src/app/(main)/timer/page.tsx
+++ src/app/(main)/timer/page.tsx
@@ -5,27 +5,41 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="w-full h-full py-4 overflow-y-scroll scrollbar-hide">
+    <div className="w-full h-full py-4 overflow-y-scroll scrollbar-hide space-y-2">
       <p className="text-sm text-center text-muted-foreground">
         tip. 공부를 하면 보유중인 알들을 부화시킬 수 있어요
       </p>
-      <div className="px-10">
+
+      {/* Character Section */}
+      <div className="w-4/5 mx-auto">
         <Image width={500} height={500} src="/orange.png" alt="orange" />
       </div>
-      <p className="text-center text-sm font-bold pb-4">
-        보유중인 알 (최대 4개)
-      </p>
-      <div className="grid grid-cols-4 px-4 gap-1">
-        {dummyUserEggs.map((egg, idx) => {
-          return (
-            <EggCard
-              key={idx}
-              imgSrc={egg.imgSrc}
-              restSeconds={egg.restSeconds}
-            />
-          );
-        })}
-        {dummyUserEggs.length < 4 ? <EggAddCard /> : null}
+
+      {/* Egg Section */}
+      <div className="px-4">
+        <p className="text-center text-sm font-bold pb-4">
+          보유중인 알 (최대 4개)
+        </p>
+        <div className="grid grid-cols-4 gap-1">
+          {dummyUserEggs.map((egg, idx) => {
+            return (
+              <EggCard
+                key={idx}
+                imgSrc={egg.imgSrc}
+                restSeconds={egg.restSeconds}
+              />
+            );
+          })}
+          {dummyUserEggs.length < 4 ? <EggAddCard /> : null}
+        </div>
+      </div>
+      {/* Streak Section */}
+      <div className="flex flex-col justify-center px-4 pt-4">
+        <p className="text-center text-sm font-bold pb-4">스트릭 (임시)</p>
+        <img
+          src="http://mazandi.herokuapp.com/api?handle=lcwoo3145&theme=warm"
+          alt="zandi"
+        />
       </div>
     </div>
   );
